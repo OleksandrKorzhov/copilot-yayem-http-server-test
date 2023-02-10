d7b946b14a603caae2e3f23a23651838525c8335
Oleksandr Korzhov <another.fullstack.dev@gmail.com>
2023-02-10 16:25:02 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(morgan("dev"))
+
+const router = new express.Router();
+
+router.get("/api/health", (req, res) => {
+  res.sendStatus(200);
+});
+
+router.get("/api/hello-world", (req, res, next) => {
+  res.send("hello world");
+});
+
+router.all("/", (req, res) => {
+  res.sendStatus(404);
+})
+
+app.use((err, req, res, next) => {
+  res.sendStatus(500);
+});
+
+app.use(router);
+
+app.listen(3200, () => {
+  console.log("App listens onf 3200 port");
+});
